52696976ac7891b4597a94c4812ad54098a797fd
Mi Taseen <114951279+InferiorAK@users.noreply.github.com>
2023-05-25 12:03:54 +0600

    Update spinning_cursor.py

--- Python/spinning_cursor.py
+++ Python/spinning_cursor.py
@@ -1,11 +1,14 @@
 # Author: InferiorAK
-## Spinning Cursor in Python
+# Spinning Cursor in Python
 
 from time import sleep as ghum
 
-symbols = ["|","/","-","\\"]
+def spin():
+	# symbols = ["|","/","-","\\"]
+	symbols = ["| Loading. Please Wait", "/ Loading. Please Wait", "- Loading. Please Wait", "\\ Loading. Please Wait"]
+	while True:
+		for symbol in symbols:
+			print(f"\r{symbol}", end="")
+			ghum(0.5)
 
-for i in range(10):
-	for symbol in symbols:
-		print(f"\r{symbol}", end = "")
-		ghum(0.5)
+spin()
